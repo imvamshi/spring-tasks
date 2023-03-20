df4a00f8020ccf06cf88348c2851f3e5fc4a8a12
raghu <iamraghu@live.com>
2023-03-20 04:04:04 -0400

    removed unused library headers

--- src/main/java/com/rv/springproject/SpringProjectApplication.java
+++ src/main/java/com/rv/springproject/SpringProjectApplication.java
@@ -1,7 +1,5 @@
 package com.rv.springproject;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class SpringProjectApplication {
